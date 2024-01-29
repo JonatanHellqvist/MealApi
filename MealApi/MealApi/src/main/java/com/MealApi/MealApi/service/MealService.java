f0b304ee089f5bbd8e3215c562b69c8430a85827
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-01-29 14:39:25 +0100

    Added MealService.java

---
+++ MealApi/MealApi/src/main/java/com/MealApi/MealApi/service/MealService.java
@@ -0,0 +1,41 @@
+package com.MealApi.MealApi.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.MealApi.MealApi.api.model.Meal;
+
+@Service
+public class MealService {
+
+	private List<Meal> mealList;
+	
+	public MealService() {
+		mealList = new ArrayList<>();
+		mealList.add(new Meal("Tikamasala", "gott"));
+		mealList.add(new Meal("Kött", "gött mos"));
+		mealList.add(new Meal("Taco", "gött mos"));
+		mealList.add(new Meal("Gröt", "gött mos"));
+		mealList.add(new Meal("PyttiPanna", "gött mos"));
+		
+	}
+
+	public Meal getMeal(UUID id) {
+		return mealList.stream()
+		.filter(meal -> id.equals(meal.getId()))
+		.findFirst()
+		.orElse(null);
+	}
+
+	public List<Meal> getMeals() {
+		return null;
+	}
+
+	public Meal addMeal(Meal meal) {
+		mealList.add(meal);
+		return null;
+	}
+}
