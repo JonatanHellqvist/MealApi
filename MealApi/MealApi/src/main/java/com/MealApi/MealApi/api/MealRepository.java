aad68077dfb53e01ccd07d61a8d89601d37ee58e
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-01-31 21:59:14 +0100

    Funktion för comments
    Patchmapping comment + crud operation för comment

--- MealApi/MealApi/src/main/java/com/MealApi/MealApi/api/MealRepository.java
+++ MealApi/MealApi/src/main/java/com/MealApi/MealApi/api/MealRepository.java
@@ -1,11 +1,19 @@
 package com.MealApi.MealApi.api;
 
-import java.util.UUID;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.MealApi.MealApi.api.model.Meal;
 
+import jakarta.transaction.Transactional;
+
 public interface MealRepository extends CrudRepository <Meal,Integer> {
+
+	@Transactional
+	@Modifying
+	@Query("UPDATE Meal m SET m.comment = :newComment WHERE m.id = :id")
+	void editComment(@Param ("newComment") String newComment,@Param("id") int id);
 
 }
