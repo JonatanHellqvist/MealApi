ba4cc578e3ae82523744712bda3ac8ec4ffe61aa
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-02-04 21:22:11 +0100

    readme

--- MealApi/MealApi/src/main/java/com/MealApi/MealApi/api/model/Meal.java
+++ MealApi/MealApi/src/main/java/com/MealApi/MealApi/api/model/Meal.java
@@ -1,10 +1,6 @@
 package com.MealApi.MealApi.api.model;
 
-import java.util.UUID;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
